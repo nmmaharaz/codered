b6330d7102ff112343ff459fb986f8935e227c3e
nmmaharaz <nmmaharaz@gmail.com>
2025-05-04 12:25:05 +0600

    change code

--- app/community/page.jsx
+++ app/community/page.jsx
@@ -1,31 +1,37 @@
-import axios from "axios";
+export const dynamic = "force-dynamic";
+
 import CommunityPage from "./component/CommunityPage";
 import LoadingPage from "../loading";
 
-
-
 const fetchAllGroupData = async () => {
   try {
-    const { data } = await axios.get(`${process.env.NEXTAUTH_URL}/api/community`);
-    return data;
+    const baseUrl = process.env.NEXTAUTH_URL;
+    const res = await fetch(`${baseUrl}/api/community`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch groups");
+    }
+
+    return await res.json();
   } catch (error) {
     console.error("Error fetching group info:", error);
     return null;
   }
 };
+
 export default async function GroupList() {
-  const groups =await fetchAllGroupData();
+  const groups = await fetchAllGroupData();
   console.log(groups, "groups==>");
 
   if (!groups) {
     return (
       <div className="text-center text-gray-400">
-        <LoadingPage></LoadingPage>
+        <LoadingPage />
       </div>
     );
   }
 
-  return (
-    <CommunityPage groups={groups}></CommunityPage>
-  );
+  return <CommunityPage groups={groups} />;
 }
